1604da2439856614bbc3828a1daa209a7bb5d75a
unknown <nishadsanjana075@gmail.com>
2026-06-20 10:27:56 +0530

    first message

---
+++ homework/day6/script_1.js
@@ -0,0 +1,9 @@
+var f = document.getElementsById("first");
+console.log(f);
+f.innerHTML = "hello";
+
+let e = document, querySelector('div.decond.box');
+console.log(e);
+
+f = document, querySelectorAll("p.second");
+console.log(f);
